f305d9a914116eb1b02381c335bf93f0bb1071bd
Cabrera <Elvis.Cabrera@ingrammicro.com>
2018-05-10 13:21:15 -0500

    make it better

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ def statistics():
 		results.append(dict(zip(columns,row)))
 
 
-	return jsonify(DecimalEncoder().encode(results[0]))
+	return json.loads(jsonify(DecimalEncoder().encode(results[0])))
 
 
 if __name__ == '__main__':
